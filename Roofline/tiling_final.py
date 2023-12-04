3a13df059947ae7eda66dd6f34323ccac197caab
zzhang855 <123493963+zzhang855@users.noreply.github.com>
2023-12-04 16:49:23 -0600

    Update tiling_final.py

--- Roofline/tiling_final.py
+++ Roofline/tiling_final.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def gcd(a, b):
     while b:
@@ -63,71 +64,90 @@ def tiling(Poy, Pof, pixel_datawidth, weight_datawidth, Nif, Nox, Noy, Nkx, Nky,
     Tky = Nky
     Tif = Nif
     Tox = Nox
+    Tix = (Tox-1)*S + Nkx
     CONVs = len(Nif)
     Toys = [0]*CONVs
     Tofs = [0]*CONVs
-    Toy_interm = np.array([0]*CONVs)
-    Tof_interm = np.array([0]*CONVs)
+    Toy_high = np.array([0]*CONVs)
+    Tof_high = np.array([0]*CONVs)
+    Toy_low = np.array([0]*CONVs)
+    Tof_low = np.array([0]*CONVs)
     for L in range (CONVs):
-        Toys[L] = sorted(find_possible_tiling(Noy[L], Poy)) #y
-        Tofs[L] = sorted(find_possible_tiling(Nof[L], Pof)) #f
-        Toy_interm[L] = Toys[L][0] # assume smallest Noy buffer 
-        Tof_interm[L] = Nof[L] #assume choose fully buffer Nof 
-    words_px = [0]*CONVs
-    words_wt = [0]*CONVs
-    for L in range (CONVs):
-        Tix = (Tox[L]-1)*S + Nkx
-        Tiy = (Toy_interm-1)*S + Nky
-        words_px[L] = Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Toy_interm[L] * Tof_interm[L]
-        words_wt[L] = Nof[L] * Tif[L] * Tkx[L] * Tky[L]
-    bits_BUF_px_wt = max(words_px)*pixel_datawidth + max(words_wt) * weight_datawidth
-    print(f'\ninitial: {bits_BUF_px_wt}\n')
-    switched = [0] * CONVs
-    Tix = (Tox-1)*S + Nkx
-    Tiy = (Noy-1)*S + Nky
-    while True:   
-        temp_words_px = words_px
-        temp_words_wt = words_wt
-        switch_index = temp_words_wt.index(max(temp_words_wt))
-        temp_words_px[switch_index] = Tix[switch_index] * Tiy[switch_index] * Tif[switch_index] + Tox[switch_index] * Noy[switch_index] * Tofs[switch_index][0]
-        temp_words_wt[switch_index] = Tofs[switch_index][0] * Tif[switch_index] * Tkx[switch_index] * Tky[switch_index]
-        if max(temp_words_px)*pixel_datawidth + max(temp_words_wt) * weight_datawidth < bits_BUF_px_wt:
-            words_px = temp_words_px
-            words_wt = temp_words_wt
-            Toy_interm[switch_index] = Noy[switch_index]
-            Tof_interm[switch_index] = Tofs[switch_index][0]
-            switched[switch_index] = 1
-            bits_BUF_px_wt = max(temp_words_px)*pixel_datawidth + max(temp_words_wt) * weight_datawidth
-            print('SWITCHING')
-            print(switched)
+        Toys[L] = sorted(find_possible_tiling(Noy[L], Poy))
+        Tofs[L] = sorted(find_possible_tiling(Nof[L], Pof))
+        Toy_low[L] = Toys[L][0]
+        Toy_high[L] = Noy[L]
+        Tof_low[L] = Tofs[L][0]
+        Tof_high[L] = Nof[L]
+        
+    words_px_high = np.array([0]*CONVs)
+    words_wt_high = np.array([0]*CONVs)
+    words_px_low = np.array([0]*CONVs)
+    words_wt_low = np.array([0]*CONVs)
+    
+    Tiy = (Toy_high-1)*S + Nky
+    words_px_high = Tix * Tiy * Tif + Tox * Toy_high * Tof_low * pixel_datawidth
+    words_wt_low = Tof_low * Tif * Tkx * Tky * weight_datawidth
+    
+    Tiy = (Toy_low-1)*S + Nky
+    words_px_low = Tix * Tiy * Tif + Tox * Toy_low * Tof_high * pixel_datawidth
+    words_wt_high = Tof_high * Tif * Tkx * Tky * weight_datawidth
+    
+    words_px = np.array(words_px_low)
+    words_wt = np.array(words_wt_high)
+    
+    Toy = np.array(Toy_low)
+    Tof = np.array(Tof_high)
+    switched = np.array([0] * CONVs)
+    switched_temp = np.array([0] * CONVs)
+    bits_BUF_px_wt = max(words_px_low) + max(words_wt_high)
+    #print(f'\nfinal: {bits_BUF_px_wt}\n')
+    while True:
+        Toy_temp = np.array(Toy)
+        Tof_temp = np.array(Tof)
+        words_px_temp = np.array(words_px)
+        words_wt_temp = np.array(words_wt)
+        words_px_max = max(words_px_temp)
+        words_wt_max = max(words_wt_temp)
+        bits_BUF_px_wt = words_px_max + words_wt_max
+        for L in range(CONVs):
+            if words_wt_temp[L] == words_wt_max:
+                words_px_temp[L] = words_px_high[L] 
+                Toy_temp[L] = Toy_high[L]
+                words_wt_temp[L] = words_wt_low[L]
+                Tof_temp[L] = Tof_low[L]
+                switched_temp[L] = 1
+        if max(words_px_temp) + max(words_wt_temp) < bits_BUF_px_wt:
+            words_px = np.array(words_px_temp)
+            words_wt = np.array(words_wt_temp)
+            Toy = np.array(Toy_temp)
+            Tof = np.array(Tof_temp)
+            switched = np.array(switched_temp)
         else:
             break
-            
+#     print(f'Toy: {Toy}')
+#     print(f'Tof: {Tof}')
     max_words_px = max(words_px)
-    #print(f'max_words_px: {max_words_px}')
     max_words_wt = max(words_wt)
     Tix = (Tox-1)*S + Nkx
     for L in range (CONVs):
         if switched[L] == 1:
             for i in range(len(Tofs[L])):
                 Tiy = (Noy-1)*S + Nky
-                if Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Toy_interm[L] * Tofs[L][i] <= max_words_px and Tofs[L][i] * Tif[L] * Tkx[L] * Tky[L] <= max_words_wt:
-                    Tof_interm[L] = Tofs[L][i]
-                    #print(f'working 1 {L}')
+                if Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Toy[L] * Tofs[L][i] <= max_words_px and Tofs[L][i] * Tif[L] * Tkx[L] * Tky[L] <= max_words_wt:
+                    Tof[L] = Tofs[L][i]
                 else:
                     break
         else:
             for i in range(len(Toys[L])):
                 Tiy = (Toys[L][i]-1)*S + Nky
-                #print(f'words_px = {Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Tofs[L][i] * Tof_interm[L]} {L}')
-                if Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Tofs[L][i] * Tof_interm[L] <= max_words_px:
-                    Toy_interm[L] = Toys[L][i]
-                    #print(f'working 2 {L}')
+                if Tix[L] * Tiy[L] * Tif[L] + Tox[L] * Tofs[L][i] * Tof[L] <= max_words_px:
+                    Toy[L] = Toys[L][i]
                 else:
                     break
-    print(f'\nfinal: {bits_BUF_px_wt}\n')
-    print(f'Toy: {Toy_interm}')
-    print(f'Tof: {Tof_interm}')
+    #print(f'\nfinal: {bits_BUF_px_wt}\n')
+#     print(f'Toy: {Toy}')
+#     print(f'Tof: {Tof}')
     return
 
 def optimize(pixel_datawidth, weight_datawidth, Nif, Nox, Noy, Nkx, Nky, Nof, S, DSP):
@@ -136,12 +156,12 @@ def optimize(pixel_datawidth, weight_datawidth, Nif, Nox, Noy, Nkx, Nky, Nof, S,
 
 pixel_datawidth = 1
 weight_datawidth = 1
-Nox = np.array([64,32,16,8,4])
-Noy = np.array([64,32,16,8,4])
-Nof = np.array([40,80,160,320,640])
-S = np.array([1,1,1,1,1])
-DSP = 2000
-Nkx = np.array([3,4,5,6,7])
-Nky = np.array([3,4,5,6,7])
-Nif = np.array([8,8,8,8,8])
+Nox = np.array([224,224,112,112,56,56,56,28,28,28,14,14,14])
+Noy = np.array([224,224,112,112,56,56,56,28,28,28,14,14,14])
+Nof = np.array([64,64,128,128,256,256,256,512,512,512,512,512,512])
+S = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1])
+Nkx = np.array([3,3,3,3,3,3,3,3,3,3,3,3,3])
+Nky = np.array([3,3,3,3,3,3,3,3,3,3,3,3,3])
+Nif = np.array([3,64,128,128,256,256,256,512,512,512,512,512,512])
+DSP = 3000
 optimize(pixel_datawidth, weight_datawidth, Nif, Nox, Noy, Nkx, Nky, Nof, S, DSP)
